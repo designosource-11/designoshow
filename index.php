716eb4faab71941ac0128109f9cdaf4b7c230c0b
SarahOorts <r0657122@student.thomasmore.be>
2022-05-12 13:05:31 +0200

    fix: renamed index from html to php file

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/styles.css"></link>
+    <title>Designoshow 2022</title>
+</head>
+<body>
+    <header>
+        <img class="logo" src="assets/logo-designosource-wit.svg" alt="logo">
+        <div class="header_text">
+            <p>Get your *ss over to</p>
+            <p>The Designoshow</p>
+            <p>21 juni, 18u30 @Stadschouwburg, Mechelen</p>
+        </div>
+        <div class="fien">
+            <div class="fien_1"></div>
+            <div class="fien_2"></div>
+            <div class="fien_3"></div>
+        </div>
+        <div class="sarah">
+            <div class="sarah_1"></div>
+            <div class="sarah_2"></div>
+            <div class="sarah_3"></div>
+        </div>
+        <div class="brian">
+            <div class="brian_1"></div>
+            <div class="brian_2"></div>
+            <div class="brian_3"></div>
+        </div>
+        <div class="nicolas">
+            <div class="nicolas_1"></div>
+            <div class="nicolas_2"></div>
+            <div class="nicolas_3"></div>
+        </div>
+        <div class="bailey">
+            <div class="bailey_1"></div>
+            <div class="bailey_2"></div>
+            <div class="bailey_3"></div>
+        </div>
+        <div class="bastien">
+            <div class="bastien_1"></div>
+            <div class="bastien_2"></div>
+            <div class="bastien_3"></div>
+        </div>
+        <div class="stefan">
+            <div class="stefan_1"></div>
+            <div class="stefan_2"></div>
+            <div class="stefan_3"></div>
+        </div>
+        <div class="nick">
+            <div class="nick_1"></div>
+            <div class="nick_2"></div>
+            <div class="nick_3"></div>
+        </div>
+        <div class="stephanie">
+            <div class="stephanie_1"></div>
+            <div class="stephanie_2"></div>
+            <div class="stephanie_3"></div>
+        </div>
+        <div class="ilya">
+            <div class="ilya_1"></div>
+            <div class="ilya_2"></div>
+            <div class="ilya_3"></div>
+        </div>
+        <div class="nathalie">
+            <div class="nathalie_1"></div>
+            <div class="nathalie_2"></div>
+            <div class="nathalie_3"></div>
+        </div>
+        <div class="sign_up">
+            <p>Sign up</p>
+        </div>
+    </header>
+    <div class="sign_up">
+        <h2>Signosaurus up</h2>
+        <p>Be there or be rectangular. Sign up for dino’s performing live on stage.</p>
+        <form action="">
+            <input type="text" name="" id="">
+            <input type="text" name="" id="">
+            <input type="text" name="" id="">
+            <input type="checkbox" name="" id="">
+            <div class="raawrcaptcha">
+                <input type="checkbox" name="" id="">
+                <p>I'm not a dino</p>
+                <img src="" alt="">
+            </div>
+            <input class="button" type="button" name="" id="">
+        </form>
+    </div>
+    <div class="program">
+        <h2>The program</h2>
+        <p>Write something funny here pls nicolas. While ur at it change the headline too.</p>
+        <div class="schedule">
+            <div class="schedule_item">
+                <p class="time">18u30</p>
+                <p class="description">Doors open @Stadschouwburg, Mechelen</p>
+                <p class="extra">Something dumb</p>
+            </div>
+            <div class="schedule_item">
+                <p class="time">19u</p>
+                <p class="description">Start Dinoshow</p>
+                <p class="extra">Some funny text</p>
+            </div>
+            <div class="schedule_item">
+                <p class="time">21u</p>
+                <p class="description">Einde show & start receptie</p>
+                <p class="extra">More funny text</p>
+            </div>
+            <div class="schedule_item">
+                <p class="time">22u30</p>
+                <p class="description">Einde</p>
+                <p class="extra">Ya you get it</p>
+            </div>
+        </div>
+    </div>
+    <div class="blocks">
+        <div class="dresscode">
+            <h2>A dresscode, rlly?</h2>
+            <p>Old school red carpet chique with a touch of crazy. Win a prize with the best outfit</p>
+            <a href="#">View outfit moodboard</a>
+        </div>
+        <div class="convinced">
+            <h2>Convinced now?</h2>
+            <p>Some dumb text Some dumb</p>
+            <p class="button"></p>
+        </div>
+    </div>
+</body>
+</html>
