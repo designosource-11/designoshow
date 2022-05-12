1fbdbf9c1305ff47b080458af947f9d968998876
SarahOorts <r0657122@student.thomasmore.be>
2022-05-12 16:49:57 +0200

    feat: created js file for hover effect images

---
+++ index.js
@@ -0,0 +1,109 @@
+document.querySelector(".fien").addEventListener("mouseenter", ()=>{
+    document.querySelector(".fien_2").classList.remove("hidden");
+    document.querySelector(".fien_3").classList.remove("hidden");
+});
+
+document.querySelector(".fien").addEventListener("mouseleave", ()=>{
+    document.querySelector(".fien_2").classList.add("hidden");
+    document.querySelector(".fien_3").classList.add("hidden");
+});
+
+document.querySelector(".sarah").addEventListener("mouseenter", ()=>{
+    document.querySelector(".sarah_2").classList.remove("hidden");
+    document.querySelector(".sarah_3").classList.remove("hidden");
+});
+
+document.querySelector(".sarah").addEventListener("mouseleave", ()=>{
+    document.querySelector(".sarah_2").classList.add("hidden");
+    document.querySelector(".sarah_3").classList.add("hidden");
+});
+
+document.querySelector(".ilya").addEventListener("mouseenter", ()=>{
+    document.querySelector(".ilya_2").classList.remove("hidden");
+    document.querySelector(".ilya_3").classList.remove("hidden");
+});
+
+document.querySelector(".ilya").addEventListener("mouseleave", ()=>{
+    document.querySelector(".ilya_2").classList.add("hidden");
+    document.querySelector(".ilya_3").classList.add("hidden");
+});
+
+document.querySelector(".bailey").addEventListener("mouseenter", ()=>{
+    document.querySelector(".bailey_2").classList.remove("hidden");
+    document.querySelector(".bailey_3").classList.remove("hidden");
+});
+
+document.querySelector(".bailey").addEventListener("mouseleave", ()=>{
+    document.querySelector(".bailey_2").classList.add("hidden");
+    document.querySelector(".bailey_3").classList.add("hidden");
+});
+
+document.querySelector(".bastien").addEventListener("mouseenter", ()=>{
+    document.querySelector(".bastien_2").classList.remove("hidden");
+    document.querySelector(".bastien_3").classList.remove("hidden");
+});
+
+document.querySelector(".bastien").addEventListener("mouseleave", ()=>{
+    document.querySelector(".bastien_2").classList.add("hidden");
+    document.querySelector(".bastien_3").classList.add("hidden");
+});
+
+document.querySelector(".nick").addEventListener("mouseenter", ()=>{
+    document.querySelector(".nick_2").classList.remove("hidden");
+    document.querySelector(".nick_3").classList.remove("hidden");
+});
+
+document.querySelector(".nick").addEventListener("mouseleave", ()=>{
+    document.querySelector(".nick_2").classList.add("hidden");
+    document.querySelector(".nick_3").classList.add("hidden");
+});
+
+document.querySelector(".nicolas").addEventListener("mouseenter", ()=>{
+    document.querySelector(".nicolas_2").classList.remove("hidden");
+    document.querySelector(".nicolas_3").classList.remove("hidden");
+});
+
+document.querySelector(".nicolas").addEventListener("mouseleave", ()=>{
+    document.querySelector(".nicolas_2").classList.add("hidden");
+    document.querySelector(".nicolas_3").classList.add("hidden");
+});
+
+document.querySelector(".nathalie").addEventListener("mouseenter", ()=>{
+    document.querySelector(".nathalie_2").classList.remove("hidden");
+    document.querySelector(".nathalie_3").classList.remove("hidden");
+});
+
+document.querySelector(".nathalie").addEventListener("mouseleave", ()=>{
+    document.querySelector(".nathalie_2").classList.add("hidden");
+    document.querySelector(".nathalie_3").classList.add("hidden");
+});
+
+document.querySelector(".brian").addEventListener("mouseenter", ()=>{
+    document.querySelector(".brian_2").classList.remove("hidden");
+    document.querySelector(".brian_3").classList.remove("hidden");
+});
+
+document.querySelector(".brian").addEventListener("mouseleave", ()=>{
+    document.querySelector(".brian_2").classList.add("hidden");
+    document.querySelector(".brian_3").classList.add("hidden");
+});
+
+document.querySelector(".stephanie").addEventListener("mouseenter", ()=>{
+    document.querySelector(".stephanie_2").classList.remove("hidden");
+    document.querySelector(".stephanie_3").classList.remove("hidden");
+});
+
+document.querySelector(".stephanie").addEventListener("mouseleave", ()=>{
+    document.querySelector(".stephanie_2").classList.add("hidden");
+    document.querySelector(".stephanie_3").classList.add("hidden");
+});
+
+document.querySelector(".stefan").addEventListener("mouseenter", ()=>{
+    document.querySelector(".stefan_2").classList.remove("hidden");
+    document.querySelector(".stefan_3").classList.remove("hidden");
+});
+
+document.querySelector(".stefan").addEventListener("mouseleave", ()=>{
+    document.querySelector(".stefan_2").classList.add("hidden");
+    document.querySelector(".stefan_3").classList.add("hidden");
+});
